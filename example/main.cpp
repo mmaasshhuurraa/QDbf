2604115f344a0b525fb2413622ab348dfc0fc088
Ivan Pinezhaninov <ivan.pinezhaninov@gmail.com>
2017-02-07 11:27:02 +0300

    License update

--- example/main.cpp
+++ example/main.cpp
@@ -1,3 +1,26 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 Ivan Pinezhaninov <ivan.pinezhaninov@gmail.com>
+**
+** This file is part of the QDbf - Qt DBF library
+** which can be found at https://github.com/IvanPinezhaninov/qdbf/.
+**
+** This library is free software; you can redistribute it and/or modify it
+** under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation; either version 2.1 of the License,
+** or (at your option) any later version.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this library. If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+
 #include "mainwindow.h"
 
 #include <QApplication>
